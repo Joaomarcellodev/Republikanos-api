e8a6b3a23db298d6920a09e53400cced96b8c2f8
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-12-28 16:27:15 -0300

    criando e adicionando projeto ao repositório

---
+++ src/main/java/br/uespi/republikanos/republikanos_api/RepublikanosApiApplication.java
@@ -0,0 +1,13 @@
+package br.uespi.republikanos.republikanos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepublikanosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepublikanosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uespi/republikanos/republikanos_api/RepublikanosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uespi.republikanos.republikanos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepublikanosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
